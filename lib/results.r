408af37513d82356cbd8b78416eb2cff00c8b939
kellijohnson <kellifayejohnson@gmail.com>
2014-10-21 07:49:49 -0700

    update results files to include CS runs

--- lib/results.r
+++ lib/results.r
@@ -25,12 +25,20 @@ wd.curr <- getwd()
 library(ggplot2)
 resultfiles <- dir("c:/users/kelli/dropbox/estgrowth", 
                    pattern = ".csv", full.names = TRUE)
-results <- lapply(resultfiles, read.csv)
+
+scalars <- do.call("rbind", lapply(grep("scalar", resultfiles, value = TRUE), read.csv))
+ts <- do.call("rbind", lapply(grep("ts", resultfiles, value = TRUE), read.csv))
 
 setwd(dir.results)
 
-results_re <- calculate_re(results[[1]], FALSE)
+results_re <- calculate_re(scalars, FALSE)
 
-plot_scalar_boxplot(results_re, x = "L", y = "SizeSel_1P_1_Fishery_re", 
+plot_scalar_boxplot(results_re, x = "L", y = "L_at_Amin_Fem_GP_1_re", 
                     vert="A", horiz="E", rel=FALSE, axes.free=FALSE) + 
-xlab("# of length data") + ylab("relative error PARAMETER")
+xlab("Length comps for fishery and survey vs. just fishery") + ylab("relative error: Length at A min")
+
+names(results_re)
+
+hist(scalars$depletion_em)
+
+any(is.na(scalars$depletion_em))
