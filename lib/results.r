0a027d54e5bae6a9d588e02e1600fcc11a42c1f3
kellijohnson <kellifayejohnson@gmail.com>
2015-02-12 03:47:45 -0800

    Add time-series plot to results.r

--- lib/results.r
+++ lib/results.r
@@ -28,7 +28,7 @@ wd.curr <- getwd()
 setwd(wd.curr)
 setwd(dir.results)
 resultfiles <- dir(dir.dropbox, pattern = ".csv", full.names = TRUE)
-scalars <- do.call("rbind", lapply(grep("scalar", resultfiles, value = TRUE), 
+scalars <- do.call("rbind", lapply(grep("scalar", resultfiles, value = TRUE),
                    read.csv))
 ts <- do.call("rbind", lapply(grep("ts", resultfiles, value = TRUE), read.csv))
 # Calculate the relative error for scalars
@@ -41,17 +41,13 @@ results_re <- calculate_re(scalars, FALSE)
 ###############################################################################
 ###############################################################################
 # Change E0:5 to the following short names
-levels(results_re$E) <- c("fix", "int", "ext", "ext_CV", "ext_LK")
-levels(ts$E) <- c("fix", "int", "ext", "ext_CV", "ext_LK")
+levels(results_re$E) <- c("fix", "int", "ext")
+levels(ts$E) <- c("fix", "int", "ext")
 
-# Change S0:1 to the following short names
-levels(results_re$S) <- c("asymp", "dome")
-levels(ts$S) <- c("asymp", "dome")
-
-# Change how A is plotted, with A1 being last because it is the case with 
+# Change how A is plotted, with A1 being last because it is the case with
 # no age data
-results_re$A <- factor(results_re$A, levels = c("A0", "A2", "A3", "A4", "A5", "A1"))
-ts$A <- factor(ts$A, levels = c("A0", "A2", "A3", "A4", "A5", "A1"))
+results_re$A <- factor(results_re$A, levels = c("A10", "A30", "A31", "A0"))
+ts$A <- factor(ts$A, levels = c("A10", "A30", "A31", "A0"))
 
 ###############################################################################
 ###############################################################################
@@ -66,13 +62,15 @@ my.horiz2 <- "D"
 my.vert <- "A"
 my.vert2 <- "C"
 my.x <- "L"
-data.plot <- subset(results_re, A %in% c("A10", "A30", "A31") & 
-                    L %in% c("L10", "L30", "L31") & D %in% c("D10", "D30") & C %in% c("C10","C20"))
+data.plot <- subset(results_re, A %in% c("A0", "A10", "A30", "A31") &
+                    L %in% c("L10", "L30", "L31") &
+                    C %in% c("C0", "C10","C20") & D %in% c("D0", "D10", "D20"))
 data.plot<-results_re
-ts <- subset(ts, A %in% c("A10", "A30", "A31") & 
-             L %in% c("L10", "L30", "L31") & D %in% c("D10", "D30") & C %in% c("C10","C20"))
+ts <- subset(ts, A %in% c("A10", "A30", "A31") &
+             L %in% c("L10", "L30", "L31") &
+             C %in% c("C0", "C10","C20") & D %in% c("D0", "D10", "D20"))
 
-termSSB <- with(subset(ts, year == max(year)), 
+termSSB <- with(subset(ts, year == max(year)),
                 ((SpawnBio_om - SpawnBio_em) / SpawnBio_om))
 termSSB <- cbind(termSSB, subset(ts, year == max(year)))
 
@@ -82,94 +80,110 @@ termSSB <- cbind(termSSB, subset(ts, year == max(year)))
 #### Plots
 ###############################################################################
 ###############################################################################
-plot_scalar_boxplot(data.plot, x = my.x, y = "CV_old_Fem_GP_1_re", 
+plot_scalar_boxplot(data.plot, x = my.x, y = "CV_old_Fem_GP_1_re",
                     vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: cv old")
 ggsave("cvold.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "CV_young_Fem_GP_1_re", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(data.plot, x = my.x, y = "CV_young_Fem_GP_1_re",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: cv young")
 ggsave("cvyoung.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "L_at_Amin_Fem_GP_1_re", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(data.plot, x = my.x, y = "L_at_Amin_Fem_GP_1_re",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: length at A min")
 ggsave("latamin.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "L_at_Amax_Fem_GP_1_re", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(data.plot, x = my.x, y = "L_at_Amax_Fem_GP_1_re",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: length at A max")
 ggsave("latamax.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "VonBert_K_Fem_GP_1_re", 
+plot_scalar_boxplot(data.plot, x = my.x, y = "VonBert_K_Fem_GP_1_re",
                     vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
-                    horiz2 = my.horiz2, 
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    horiz2 = my.horiz2,
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: Vonb K")
 ggsave("vonbk.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "depletion_re", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(data.plot, x = my.x, y = "depletion_re",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: depletion")
 ggsave("depletion.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(data.plot, x = my.x, y = "SSB_MSY_re", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(data.plot, x = my.x, y = "SSB_MSY_re",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: SSB at MSY")
 ggsave("ssbmsy.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(termSSB, x = my.x, y = "termSSB", 
-                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2, 
+plot_scalar_boxplot(termSSB, x = my.x, y = "termSSB",
+                    vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
-                    rel = axis.rel, axes.free = axis.val) + 
-xlab("Length comps for fishery and survey vs. just fishery") + 
+                    rel = axis.rel, axes.free = axis.val) +
+xlab("Length comps for fishery and survey vs. just fishery") +
 ylab("relative error: terminal SSB")
 ggsave("termssb.png", dpi = 300)
 dev.off()
 
 
-scalar_long <- reshape2::melt(subset(data.plot, E == "E2",
+scalar_long <- reshape2::melt(subset(data.plot, E == "fix",
   select = c("scenario", "E", "A", "L", "D", "C",
-  "replicate", "CV_old_Fem_GP_1_re", "CV_young_Fem_GP_1_re", 
-  "L_at_Amin_Fem_GP_1_re", "L_at_Amax_Fem_GP_1_re", "VonBert_K_Fem_GP_1_re")), 
+  "replicate", "CV_old_Fem_GP_1_re", "CV_young_Fem_GP_1_re",
+  "L_at_Amin_Fem_GP_1_re", "L_at_Amax_Fem_GP_1_re", "VonBert_K_Fem_GP_1_re")),
   id.vars = c("scenario", "A", "L", "D", "C", "E", "replicate"))
 scalar_long <- plyr::rename(scalar_long, c("value" = "relative_error"))
 levels(scalar_long$variable) <- c("CV_old", "CV_young", "L_min", "L_max", "K")
-ggplot(subset(scalar_long), aes(D,"relative_error")) +
-geom_boxplot(size = 0.2, outlier.size = 1) + 
-geom_hline(yintercept = 0, col = "red") + 
+ggplot(scalar_long, aes(E, "relative_error")) +
+geom_boxplot(size = 0.2, outlier.size = 1) +
+geom_hline(yintercept = 0, col = "red") +
 geom_hline(aes(yintercept = 0), lty = 2) +
-facet_grid(variable ~ A + D) + ylim(-1, 1) + theme_bw() + 
+#facet_grid(variable ~ A + D) + ylim(-1, 1) + theme_bw() +
 xlab("Length comps for fishery and survey vs. just fishery")
 ggsave("compareE2.png", dpi = 300)
 dev.off()
+
+
+###############################################################################
+###############################################################################
+#### Step
+#### Time series
+###############################################################################
+###############################################################################
+ts$SpawnBio_re <- with(ts, (SpawnBio_om - SpawnBio_em) / SpawnBio_om)
+ggplot(ts) +
+geom_boxplot(aes(year, SpawnBio_re, group = replicate), size = 0.2, outlier.size = 1) +
+xlab("year")
+
+ggplot(ts) +
+geom_line(aes(year, SpawnBio_re, group = replicate), size = 0.2, outlier.size = 1) +
+xlab("year")
 
 ###############################################################################
 ###############################################################################
@@ -178,16 +192,16 @@ dev.off()
 ###############################################################################
 ###############################################################################
 png("samples.png")
-fulldata <- lapply(do.call("rbind", 
-                    strsplit(readLines("../casefiles/lcomp0-col.txt")[1:3], 
+fulldata <- lapply(do.call("rbind",
+                    strsplit(readLines("../casefiles/lcomp0-col.txt")[1:3],
                              ";"))[, 2],
               function(x) eval(parse(text = x)))
-lessdata <- lapply(do.call("rbind", 
-                    strsplit(readLines("../casefiles/lcomp2-col.txt")[1:3], 
+lessdata <- lapply(do.call("rbind",
+                    strsplit(readLines("../casefiles/lcomp2-col.txt")[1:3],
                              ";"))[, 2],
               function(x) eval(parse(text = x)))
-mindata <- lapply(do.call("rbind", 
-                    strsplit(readLines("../casefiles/lcomp4-col.txt")[1:3], 
+mindata <- lapply(do.call("rbind",
+                    strsplit(readLines("../casefiles/lcomp4-col.txt")[1:3],
                              ";"))[, 2],
               function(x) eval(parse(text = x)))
 plot(1:100, 1:100, ylim = c(5, 42), las = 1, yaxt = "n",
@@ -200,7 +214,7 @@ points(lessdata[[3]][[1]], lessdata[[2]][[1]] - 1, pch = 1, cex = 1)
 points(lessdata[[3]][[1]], lessdata[[2]][[1]] + 3, pch = 1, cex = 1)
 points(lessdata[[3]][[2]], lessdata[[2]][[2]] + 2, pch = 19, cex = 1)
 points(mindata[[3]][[1]], mindata[[2]][[1]] - 4, pch = 1, cex = 1)
-legend(80, 25, pch = c(1,19), legend = c("Fishery", "Survey"), 
+legend(80, 25, pch = c(1,19), legend = c("Fishery", "Survey"),
        bty = "n", cex = 1.2, ncol = 1)
 text(x = 10, y = 41, labels = "L0, A0")
 text(x = 10, y = 38, labels = "L1, A2")
