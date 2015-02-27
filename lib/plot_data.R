a014861e30e1c9cfecc8289f4d336eeb3379fb94
kellijohnson <kellifayejohnson@gmail.com>
2015-02-26 21:32:26 -0800

    Change data plot from wide to long

--- lib/plot_data.R
+++ lib/plot_data.R
@@ -6,7 +6,7 @@ wd.curr <- getwd()
 full <- function(data, years = 1:100) {
   years <- data.frame("year" = years)
   if (length(data[[1]]) > 1) {
-    data <- with(data,
+    data <- with(data, 
       mapply(cbind, years, Nsamp, mapply(list, fleets)))
     data <- lapply(data, function(i) {
       colnames(i) <- c("year", "n", "fleet")
@@ -55,62 +55,64 @@ data <- eval(parse(text = paste("list(",
   paste(paste("p", 1:11, sep = "."), collapse = ","), ")")))
 
 png(file.path("results", "data.png"),
-  height = 6, width = 6, units = "in", res = 200)
+  height = 12, width = 7, units = "in", res = 200)
 dens <- 50
-par(mfrow = c(8, 10), oma = c(2.5, 2, 1, 1), mar = rep(0,4))
-for (start in seq(21, 91, by = 10)) {
-sapply(start:(start + 9), function(x) {
-    data.plot <- lapply(data, function(y) {
-        temp <- y[, x]
-        return(temp)
-    })
-    data.plot <- do.call("cbind", data.plot)
-    data.plot[is.na(data.plot)] <- 0
-    if (all(data.plot == 0)) {
-      barplot(data.plot[1,1], axes = FALSE, col = "white",
-        ylim = c(0, 500), border = "white", xlim = c(0, 1),
-        width = 0.000001)
-    } else {
-    mycol <- which(apply(data.plot, 1, sum) > 0)
-    barplot(data.plot, yaxt = "n", xaxt = "n", ylim = c(0, 500),
-      border = "white",
-      col = c(2,2,3,3,4,5,6),
-      density = c(-1, dens, -1, dens, -1, -1, -1))
-    }
+myhorz <- TRUE
+par(mfcol = c(40, 2), oma = c(2.5, 2.5, 1, 1), mar = rep(0, 4),
+	xpd = TRUE)
+sapply(c(21:60, 61:100), function(x) {
+	data.plot <- lapply(data, function(y) {
+		temp <- y[, x]
+		return(temp)
+	})
+	data.plot <- do.call("cbind", data.plot)
+	data.plot[is.na(data.plot)] <- 0
+	if (all(data.plot == 0)) {
+	  barplot(data.plot[1,1], axes = FALSE, col = "white",
+	  	xlim = c(0, 500), border = "white", ylim = c(0, 1),
+	  	width = 0.000001, horiz = myhorz)
+	} else {
+	barplot(data.plot, yaxt = "n", xaxt = "n", xlim = c(0, 500),
+	  border = "white", horiz = myhorz,
+	  col = c(2,2,3,3,4,5,6), 
+	  density = c(-1, dens, -1, dens, -1, -1, -1))
+	if (x == 80) box(lwd = 2.5)
+	}
 })
-}
-ys <- c(0.89, 0.77, 0.645, 0.51, 0.42, 0.27, 0.147, 0.01)
-xs <- seq(20, 90, by = 10)
-for(start in seq_along(ys)) {
-  par(fig = c(0, 1, ys[start], ys[start] + 0.05), new = TRUE)
-  x <- seq(xs[start] + 2, xs[start] + 10,
-    by = ifelse(start < 6, 4, 2))
-  y <- rep(0.005, length(x))
-  plot(xs[start]:(xs[start] + 9), rep(1, 10),
-    type = "n", bty = "n", axes = FALSE)
-  axis(1, at = x - 0.75, x, lwd = 0.0)
-}
-par(fig = c(0,1,0,1), new = TRUE)
-    legend("topleft", bty = "o", fill = c(2,2,3,3,4,5,6),
-      xpd = TRUE, cex = 1.25,
-      border = "white", box.lwd = 0, box.col = "white",
-      density = c(NA, dens, NA, dens, NA, NA, NA),
-      legend = c("length comp: fishery", "length comp: survey",
-                 "age comp: fishery", "age comp: fishery",
-                 "cond age-at-length: survey",
-                 "mean length-at-age: fishery",
-                 "mean length-at-age: survey"))
-    mtext(side = 1, text = "year", line = 1.5, outer = TRUE)
-    mtext(side = 2, "sample size", line = -1, outer = TRUE)
 
+  par(fig = c(0, 0.5, 0, 1), new = TRUE)
+  x <- seq(20, 60, by = 5)
+  plot(rep(0.005, length(x)), x, type = "n", bty = "n", axes = FALSE)
+  text(0.003, 58, cex = 1.5, xpd = TRUE, label = "years", srt = -90)
+  axis(2, at = x - 0.75, rev(x), las = 1)
+  legend(0.003, at = 41, legend = "years")
+ 	legend("topright", inset=c(0.2,0.15), bty = "o", 
+  	  fill = c(2,2,3,3,4,5,6), 
+  	  xpd = TRUE, cex = 1.35, 
+  	  border = "white", box.lwd = 0, box.col = "white",
+  	  density = c(NA, dens, NA, dens, NA, NA, NA),
+      legend = c("length comp: fishery", "length comp: survey",
+  	             "age comp: fishery", "age comp: fishery",
+  	             "cond age-at-length: survey",
+  	             "mean length-at-age: fishery", 
+  	             "mean length-at-age: survey"))
+  	mtext(side = 3, "sample size", line = -3, outer = TRUE, adj = 0)
+
+  par(fig = c(0.5, 1, 0, 1), new = TRUE)
+  x <- seq(60, 100, by = 5)
+  plot(rep(0.5, length(x)), x, type = "n", bty = "n", axes = FALSE)
+  axis(2, at = x - 0.75, rev(x), las = 1)
+
+ 
 x <- 80
-par(fig = c(0.65,0.9,0.6,0.95), new = TRUE, mar = c(2, 2, 0, 0))
+par(fig = c(0.25,0.45,0.0,0.65), new = TRUE, mar = c(2, 2, 0, 0))
 barplot(sapply(data, function(y) {
-        temp <- y[, x]
-        temp[is.na(temp)] <- 0
-        return(temp)}), col = c(2,2,3,3,4,5,6),
+		temp <- y[, x]
+		temp[is.na(temp)] <- 0
+		return(temp)}), col = c(2,2,3,3,4,5,6), 
   density=c(-1,dens,-1,dens,-1,-1, -1), las = 1)
-mtext(side = 1, paste("year", x), line = 1.1, cex = 0.65)
+box()
+mtext(side = 3, paste("year", x), line = -2, cex = 0.95, adj = 0)
 mtext(side = 1, paste(letters[1:11], collapse = " "), cex = 0.75)
 
 dev.off()
