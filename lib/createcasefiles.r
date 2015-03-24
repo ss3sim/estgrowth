fd5643de5c23422566c8db5241af52e5b24b9930
kellijohnson <kellifayejohnson@gmail.com>
2015-03-23 17:24:31 -0700

    Remove info that is duplicated with code in the package and is not needed for creating case files

--- lib/createcasefiles.r
+++ lib/createcasefiles.r
@@ -21,31 +21,6 @@
 ###############################################################################
 wd.curr <- getwd()
 setwd(dir.cases)
-start <- 1
-end   <- 100
-
-start.fishery <- 26
-years.rup <- 40
-
-start.survey  <- start + 75
-start.fishery <- start + 25
-freq.survey  <- 2
-freq.fishery <- c(10, 4)
-
-# Information regarding sample frequency
-# Amount to cut data by (e.g., if 2 take every other point)
-reducer <- 2
-# Number of years randomly picked for mlacomps
-nmlayears <- 2
-# Number of years randomly picked for calcomps
-ncalyears <- 2
-
-# Information regarding sample intensity
-# Currently not using the lower sample size as Ono et al. (2014)
-# showed sample size really did not matter
-# lcomp and age comp sample sizes
-high <- 40
-low <- 10
 
 # Estimate CVs or not
 estCVs <- TRUE
@@ -56,14 +31,6 @@ estCVs <- TRUE
 #### Cases that are not currently being used
 ###############################################################################
 ###############################################################################
-# Change asymptotic selectivity to dome shaped in OM
-# Not currently using, but available, list with a vector of 7 parameter values to
-# add to selectivity, each species needs a vector. Amount is how much to
-# add or subtract from the double normal currently programmed as asymptotic
-# dome <- list(c(51.5 - 50.8, -4 - -3, 5.2 - 5.1, 8 - 15, 0, 0))
-# amount of random noise in selectivity for each spp - pertains to parameter 1
-tvs <- rep(0.1, length = length(my.spp))
-
 # Sequence true natural morality values for each species by 10 - 190%
 # These values will be used to change the EM values of fixed natural mortality
 # to add misspecification in the EM.
@@ -75,33 +42,6 @@ mrange <- lapply(truem, function(x) {
   temp <- x * seq(.5, 1.50, by = 0.1)
   temp[-which(temp == x)]
 })
-
-
-
-###############################################################################
-###############################################################################
-#### Step
-#### Calculate years that things happen based on inputs above
-###############################################################################
-###############################################################################
-years.fish <- end + start - start.fishery
-
-# Number of years the fishery ramps up before starting two way trip
-all.surv <- seq(start.survey, end, by = freq.survey)
-all.fish <- c(seq(start.fishery, start.fishery + 20, by = freq.fishery[1]),
-              seq(start.fishery + 30, end, by = freq.fishery[2]))
-
-less <- sapply(c("surv", "fish"), function(x) {
-                realword <- paste("all", x, sep = ".")
-                vals <- eval(parse(text = realword))
-                vals[seq.int(1L, length(vals), reducer)]
-               })
-
-years.txt <- lapply(list(as = all.surv, af = all.fish,
-                         ls = less$surv, lf = less$fish),
-                    function(x) {
-                      paste0("c(", paste(x, collapse = ", "), ")")
-                    })
 
 ###############################################################################
 ###############################################################################
