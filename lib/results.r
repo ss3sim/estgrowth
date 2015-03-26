1b1396d3d460dae4932c5d54e766847a8c7dbfee
kellijohnson <kellifayejohnson@gmail.com>
2015-03-25 21:45:22 -0700

    major changes

--- lib/results.r
+++ lib/results.r
@@ -16,6 +16,20 @@
 ###############################################################################
 ###############################################################################
 #### Step
+#### Input that could change according to users
+###############################################################################
+###############################################################################
+my.horiz <- "E"
+my.horiz2 <- "C"
+my.vert <- "A"
+my.vert2 <- "species"
+my.x <- "L"
+
+conv.crit <- 0.01
+
+###############################################################################
+###############################################################################
+#### Step
 #### Get and manipulate result files from the shared dropbox folder
 ###############################################################################
 ###############################################################################
@@ -24,18 +38,33 @@ if(!"ggplot2" %in% rownames(installed.packages())) {
 }
 library(ggplot2)
 library(ss3sim)
-source(file.path("..", "lib", "plot_functions.R"))
 
+# Find auxiliary code with plotting functions
+if (file.exists("lib", "plot_functions.R")) {
+  source(file.path("lib", "plot_functions.R"))
+} else {
+  message("Choose file that contains the plot functions.")
+  source(file.choose(new = FALSE))
+}
+
+# Set up folder structure
+if (!exists("dir.dropbox")) {
+  dir.dropbox <- choose.dir(caption = "Select the dir containing csv's")
+}
+if (!exists("dir.results")) {
+  dir.results <- choose.dir(caption = "Select plotting directory")
+}
+
+# Read in the results
 wd.curr <- getwd()
-setwd(wd.curr)
 setwd(dir.results)
-resultfiles <- list.files(dir.dropbox, pattern = ".csv",
-  full.names = TRUE, recursive = TRUE)
-scalars <- do.call("rbind",
-  lapply(grep("scalar", resultfiles, value = TRUE), read.csv))
-ts <- do.call("rbind",
-  lapply(grep("ts", resultfiles, value = TRUE), read.csv))
+resultfiles <- list.files(dir.dropbox, pattern = ".csv", full.names = TRUE)
+scalars <- read.csv(grep("scalar", resultfiles, value = TRUE), header = TRUE)
+ts <- read.csv(grep("ts", resultfiles, value = TRUE), header = TRUE)
+
 # Calculate the relative error for scalars
+scalars <- calculate_re(scalars, TRUE)
+ts <- calculate_re(ts, TRUE)
 results_re <- calculate_re(scalars, FALSE)
 
 ###############################################################################
@@ -44,16 +73,26 @@ results_re <- calculate_re(scalars, FALSE)
 #### Name cases
 ###############################################################################
 ###############################################################################
-
+scalars <- change_levels(data = scalars, group = "E",
+  old = c("E0", "E1", "E2", "E3"), new = c("fix", "int", "ext", "intCV"))
 results_re <- change_levels(data = results_re, group = "E",
-  old = c("E0", "E1", "E2"), new = c("fix", "int", "ext"))
+  old = c("E0", "E1", "E2", "E3"), new = c("fix", "int", "ext", "intCV"))
 ts <- change_levels(data = ts, group = "E",
-  old = c("E0", "E1", "E2"), new = c("fix", "int", "ext"))
+  old = c("E0", "E1", "E2", "E3"), new = c("fix", "int", "ext", "intCV"))
 
+scalars <- change_levels(data = scalars, group = "A",
+  old = c("A0", "A10", "A30"), new = c("noage", "fishage", "bothage"))
 results_re <- change_levels(data = results_re, group = "A",
   old = c("A0", "A10", "A30"), new = c("noage", "fishage", "bothage"))
 ts <- change_levels(data = ts, group = "A",
   old = c("A0", "A10", "A30"), new = c("noage", "fishage", "bothage"))
+
+scalars <- change_levels(data = scalars, group = "L",
+  old = c("L10", "L30"), new = c("fishlen", "bothlen"))
+results_re <- change_levels(data = results_re, group = "L",
+  old = c("L10", "L30"), new = c("fishlen", "bothlen"))
+ts <- change_levels(data = ts, group = "L",
+  old = c("L10", "L30"), new = c("fishlen", "bothlen"))
 
 ###############################################################################
 ###############################################################################
@@ -63,24 +102,15 @@ ts <- change_levels(data = ts, group = "A",
 ###############################################################################
 axis.val <- TRUE
 axis.rel <- TRUE
-my.horiz <- "E"
-my.horiz2 <- "C"
-my.vert <- "A"
-my.vert2 <- "F"
-my.x <- "L"
-
-ssbterm_re <- with(ts, ((SpawnBio_om - SpawnBio_em) / SpawnBio_om))
-ts <- cbind(ts, ssbterm_re)
 
 #subset for max_grad
-maxgrad <- aggregate(max_grad ~ L + A + E + C + species, data = results_re,
-  function(x) c(median(x), sum(x > 0.01)))
-data.plot <- subset(results_re, max_grad < 0.01 &
-  C %in% c("C0", "C20") & D %in% c("D0", "D10", "D20") &
-  E %in% c("ext", "int", "E3"))
-ts <- subset(ts[ts$ID %in% data.plot$ID, ],
-  C %in% c("C0", "C10","C20") & D %in% c("D0", "D10", "D20") &
-  E %in% c("ext", "int", "E3"))
+maxgrad <- aggregate(max_grad ~ L + A + E + C + F + species, data = scalars,
+  function(x) c("median" = median(x), "num" = sum(x > conv.crit)))
+# Write to file, those with > 0 above 0.01
+write.csv(maxgrad[maxgrad$max_grad[,2] > 0, ], "maxgrad.csv")
+data.plot <- subset(results_re, max_grad < conv.crit &
+  E %in% c("ext", "int", "intCV"))
+ts.plot <- subset(ts, ID %in% data.plot$ID & E %in% c("ext", "int", "intCV"))
 
 ###############################################################################
 ###############################################################################
@@ -151,7 +181,8 @@ ylab("relative error: SSB at MSY")
 ggsave("ssbmsy.png", dpi = 300)
 dev.off()
 
-plot_scalar_boxplot(ts[ts$year == max(ts$year), ], x = my.x, y = "ssbterm_re",
+plot_scalar_boxplot(ts.plot[ts.plot$year == max(ts.plot$year), ],
+                    x = my.x, y = "SpawnBio_re",
                     vert = my.vert, horiz = my.horiz, vert2 = my.vert2,
                     horiz2 = my.horiz2,
                     rel = axis.rel, axes.free = axis.val) +
@@ -168,43 +199,50 @@ dev.off()
 ###############################################################################
 growth <- c("L_at_Amin_Fem_GP_1_re", "L_at_Amax_Fem_GP_1_re",
             "VonBert_K_Fem_GP_1_re", "CV_young_Fem_GP_1_re",
-            "CV_old_Fem_GP_1_re", "SR_LN_R0_re",
+            "CV_old_Fem_GP_1_re",
             "SizeSel_1P_1_Fishery_re", "SizeSel_1P_3_Fishery_re",
             "SizeSel_2P_1_Survey_re", "SizeSel_2P_3_Survey_re")
 xnames <- c("Length at min age", "Length at max age",
-            expression(italic(K)), "CV_young",
-             "CV_old", expression(italic(R0)),
+            expression(italic(K)), "CV_young", "CV_old",
             "Fishery selectivity 1","Fishery selectivity 3",
             "Survey selectivity 1", "Survey selectivity 3")
-png("parcorrelations.png", height = 7, width = 7, units = "in", res = 200)
-par(mfrow = (c(3, 4)), mar = c(4, 0, 1, 0), oma = c(2, 2, 2, 2))
-for(ind in seq_along(growth)) {
-  x <- data.plot[data.plot$A != "noage" & data.plot$L == "L30", growth[ind]]
-  y <- ts[ts$year == max(ts$year) & ts$A != "noage" & ts$L == "L30", "ssbterm_re"]
-  mycol <- apply(data.plot[data.plot$A != "noage" & data.plot$L == "L30", ], 1, function(x) {
-    ifelse(x["C"] == "C0", x["D"], x["C"])
-  })
-  mycol <- as.factor(mycol)
-  levels(mycol) <- c("blue", "red", "green", "darkgreen")
-  mod <- lm(y ~ mycol / x + 0)
-  plot(x, y, col = mycol, ylim = c(-1, 1),
-    yaxt = ifelse(ind %in% c(1, 5, 9), "s", "n"),
-    xlab = xnames[ind])
-  cof <- coef(mod)
-    save <- rep(NA, 4)
-    for(i in 1:4){
-      abline(a = cof[i], b = cof[4 + i], col = mod$xlevels$mycol[i])
-      if (summary(mod)$coefficients[4 + i, 4] < 0.05) {
-        save[i] <- round(cof[4 + i], 3)
+
+pdf("parcorrelations.pdf")
+for (ind in seq_along(growth)) {
+  par(mfcol = c(3, 3), las = 1, mar = c(0, 0, 0, 0), oma = c(4, 4, 4, 2),
+    tck = -0.015, mgp = c(1.75, 0.45, 0), cex.axis = 1, col.axis = "black")
+  for (spp in c("flatfish", "hake", "yellow")) {
+    for (my.e in c("int", "ext", "intCV")) {
+      my.par <- growth[ind]
+      x <- subset(data.plot, A == "bothage" & L == "bothlen" & F == "F1" &
+        C != "C20" & D != "D10" & species == spp & E %in% my.e,
+        select = my.par)
+      y <- subset(ts.plot, year == 100 & A == "bothage" & L == "bothlen" &
+        F == "F1"  & C != "C20" & D != "D10" & species == spp & E %in% my.e,
+        select = "SpawnBio_re")
+      plot(x[, 1], y[, 1], xlim = c(-2, 2), ylim = c(-2, 2), axes = FALSE)
+      box(col = "black")
+      if (my.e == "int") {
+        if (spp == "flatfish") {
+          mtext(side = 3, xnames[ind], outer = TRUE, line = 2)
+        }
+        mtext(side = 3, spp)
       }
+      if (spp == "flatfish") {
+        axis(2)
+        mtext(side = 2, my.e, line = 2, las = 3)
+      }
+      mod <- lm(y[, 1] ~ x[, 1])
+      if (summary(mod)$coefficients[2, 4] < 0.05) {
+        abline(mod)
+        legend("topright", legend = round(coef(mod)[2], 3), bty = "n")
+      }
+      abline(v = 0, h = 0, lty = 2, col = gray(0.7))
+      legend("topleft", legend = paste("n =", length(x[, 1])), bty = "n")
     }
-    mtext(side = 3, text = paste(save, collapse = ", "),
-      adj = 0, line = -1.5, cex = 0.5)
+  axis(1)
+  }
 }
-mtext(side = 3, paste("Relative error in terminal SSB vs. ..."), outer = TRUE)
-plot(0,0, type = "n", axes = FALSE, bty = "n", xlab = "")
-  legend("topright", legend = c("survey cal", "lengths & ages", "fishery mla", "survey mla"),
-    col = c("blue", "red", "green", "darkgreen"), pch = 1, bty = "n")
 dev.off()
 
 ###############################################################################
@@ -213,7 +251,6 @@ dev.off()
 #### Time series
 ###############################################################################
 ###############################################################################
-ts$SpawnBio_re <- with(ts, (SpawnBio_om - SpawnBio_em) / SpawnBio_om)
 ggplot(ts[ts$E == "int", ]) +
 geom_boxplot(aes(year, SpawnBio_re, group = year)) +
 geom_hline(yintercept = 0, col = "red") +
